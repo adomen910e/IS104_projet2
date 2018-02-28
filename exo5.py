b3da8998b401acdcfd4cf7de649e24fada78c5b1
sdespretz <sdespretz@enseirb-matmeca.fr>
2018-02-28 16:54:17 +0100

    exo1 et exo5 incomplet

---
+++ exo5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from math import sqrt
+from exo1 import Cholesky 
+
+A = np.random.rand(3,3)
+A = A.dot(np.transpose(A))
+print(A)
+
+B = Cholesky(A)
+
+#C = Cholesky(A)
+
+D = np.transpose(np.linalg.inv(B)).dot(np.linalg.inv(B))
+
+condA = np.linalg.cond(A)
+cond = np.linalg.cond(D.dot(A))
+print(cond, condA)
+#assert(cond < condA)
+print(cond/condA)
