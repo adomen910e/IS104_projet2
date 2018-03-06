a36b18f7abaa448385caf1f5e77847e0a26114c1
DOMENGER Amelie <adomen910e@caudavenenum.pedago.ipb.fr>
2018-03-06 16:29:48 +0100

    remise en forme de l'exo1 partie 2

---
+++ partie2/exo1.py
@@ -0,0 +1,25 @@
+
+
+
+
+function [x] = conjgrad(A,b,x)
+    r = b - A * x;
+    p = r;
+    rsold = r' * r;
+ 
+    for i = 1:length(b)
+        Ap = A * p;
+        alpha = rsold / (p' * Ap);
+        x += alpha * p;
+        r -= alpha * Ap;
+        rsnew = r' * r;
+
+        if sqrt(rsnew) < 1e-10
+              break;
+        end
+
+        p = r + (rsnew / rsold) * p;
+        rsold = rsnew;
+
+    end
+end
