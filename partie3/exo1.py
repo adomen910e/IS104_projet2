6ebc8831d5d3718c6101b18f9da4b6d9b33a4f6f
Timothee <tcorsini@enseirb-matmeca.fr>
2018-03-09 16:47:56 +0100

    corrigé une erreur de l'exo1 parti3 (manque de cases vides dans la matrice)

--- partie3/exo1.py
+++ partie3/exo1.py
@@ -10,7 +10,7 @@ def mat_chaleur(N):
     A = np.zeros((n,n))
     for i in range(n):
         A[i,i] = -4
-        if(i+1 < n):
+        if(i+1 < n) and ((i+1)%N != 0):
             A[i+1, i] = 1
             A[i, i+1] = 1
         if(i+N < n):
