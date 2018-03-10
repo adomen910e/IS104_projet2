5eec7025b22c8d7730916434a0e51034e2f5c7ae
Domenger <domenger@MacBook-Pro-de-Domenger.local>
2018-03-10 17:45:44 +0100

    correctino simon

--- partie1/exo5.py
+++ partie1/exo5.py
@@ -2,25 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from math import sqrt
-from exo1 import Cholesky 
+from exo1 import Cholesky
+from exo4 import incomplete_cholesky
 
 def test():
     A = np.random.rand(3,3)
-    A = A.dot(np.transpose(A))
+    A = A.dot(np.transpose(A)) # On génère une matrice symétrique définie positive
+    print("Notre matrice symétrique définie positive :")
     print(A)
-    print("\n")
-
+    
     B = Cholesky(A)
-
-    #C = Cholesky(A)
-
+    
+    C = incomplete_cholesky(A)
+    
     D = np.transpose(np.linalg.inv(B)).dot(np.linalg.inv(B))
-
+    
+    E = np.transpose(np.linalg.inv(C)).dot(np.linalg.inv(C))
+    
     condA = np.linalg.cond(A)
-    cond = np.linalg.cond(D.dot(A))
-    print(cond, condA)
+    cond1 = np.linalg.cond(D.dot(A))
+    cond2 = np.linalg.cond(E.dot(A))
+    print("Le conditionnenment de M-1 . A avec la première méthode :", cond1,"Le conditionnenment avec la deuxième méthode :", cond2, "cond(A)", condA)
     print("\n")
-    #assert(cond < condA)
-    print(cond/condA)
+    print("Rapport entre le premier conditionnement et cond(A)", cond1/condA,"Rapport entre le second conditionnement et cond(A)", cond2/condA)
     print("\n")
+
+
